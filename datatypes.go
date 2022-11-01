17e7ed263652c6f49563e4fce5ad5fffe6dd1642
ThomasCZhang <112586835+ThomasCZhang@users.noreply.github.com>
2022-11-01 12:44:30 -0400

    Added some skeleton Functions

---
+++ datatypes.go
@@ -0,0 +1,22 @@
+package main
+
+type ECM struct {
+	width     float64
+	stiffness float64
+	fibres    []*Fibre
+	cells     []*Cell
+}
+
+type Cell struct {
+	radius, height, speed, integrin, shapeFactor, viscocity float64
+	position, projection                                    OrderedPair
+}
+
+type Fibre struct {
+	length, width       float64
+	position, direction OrderedPair
+}
+
+type OrderedPair struct {
+	x, y float64
+}
--- functions.go
+++ functions.go
@@ -1,1 +1,29 @@
 package main
+
+func SimulateCellMotility() {
+	// range over some number of generations
+	UpdateECM()
+}
+
+func UpdateECM() {
+	// range over all cells on ECM
+	UpdateCell()
+}
+
+func UpdateCell() {
+	UpdateAcceleration()
+	UpdateVelocity()
+	UpdatePosition()
+}
+
+func UpdateAcceleration() {
+
+}
+
+func UpdateVelocity() {
+
+}
+
+func UpdatePosition() {
+
+}
