91e3f34471013f55f238f9227fc184968ddc6782
ThomasCZhang <112586835+ThomasCZhang@users.noreply.github.com>
2022-11-26 12:56:49 -0500

    Working fibre code. Also, refactored fibre code.

--- fibre.go
+++ fibre.go
@@ -2,8 +2,9 @@ package main
 
 /*
 type Fibre struct {
-	length, width              float64
-	position, direction, pivot OrderedPair
+	length, width         float64
+	position, pivot       OrderedPair
+	direction, projection OrderedPair
 }
 */
 
@@ -13,40 +14,11 @@ import (
 
 // UpdateFibre calculates the new angle of rotation and changes the position of the fibre
 // Input: Nearest cell, matrix stiffness float64 value
-func (fibre *Fibre) UpdateFibre(cell *Cell, S float64) {
-
-	// phi (angle of rotation) = theta (angle of cell from pivot) - arcsin[(1 - 0.1*integrins*(1-stiffness)*perpendicular distance D) / hypotenuse]
-	// otherEnd := fibre.FindPivot(cell)
+func (fibre *Fibre) UpdateFibre(cell *Cell, stiffness float64) {
 	fibre.FindPivot(cell)
-	d := ComputeDistance(fibre.pivot, cell.position) // find the hypotenuse of the cell to the pivot point of the fibre
-	D := fibre.FindPerpendicularDistance(cell)       // find the perpendicular distance of the cell to the fibre
-	theta := math.Asin(D / d)                        // theta = arcsin(d / D)
-
-	phi := ComputePhi(fibre, cell, theta, d, D, S) // compute angle of rotation
-	m, b := fibre.GetLine()
-	y := FindYOnLine(cell.position.x, m, b)
-	if fibre.pivot.x < cell.position.x {
-		// If pivot is to the left of cell.
-		if cell.position.y < y {
-			// And the cell is below the line describing the fibre.
-			phi *= -1
-		}
-	} else {
-		// If Pivot is to the right of the cell.
-		if cell.position.y > y {
-			// And the cell is above the line describing the fibre.
-			phi *= -1
-		}
-	}
-
-	fibre.UpdateDirection(phi) // updates direction vector of fibre using phi
+	phi := fibre.ComputePhi(cell, stiffness) // compute angle of rotation
+	fibre.UpdateDirection(phi)               // updates direction vector of fibre using phi
 	fibre.UpdatePosition()
-	if math.IsNaN(fibre.direction.x) ||
-		math.IsNaN(fibre.direction.y) ||
-		math.IsNaN(fibre.position.x) ||
-		math.IsNaN(fibre.position.y) {
-		panic("Error in UpdateFibre. Position or Direction are NaN (Not a Number).")
-	}
 }
 
 // FindPivot:  Determines which end of the fibre will be considered the pivot with regards to a cell.
@@ -56,7 +28,6 @@ func (fibre *Fibre) UpdateFibre(cell *Cell, S float64) {
 // fibre (*Fibre) pointer to the fibre being analyzed.
 // cell (*Cell) pointer to the cell object being analyzed.
 func (fibre *Fibre) FindPivot(cell *Cell) {
-
 	endpoint1, endpoint2 := fibre.GetEndpoints()
 	//calculate the distance between the cell and each endpoint to identify the pivot
 	distance1 := ComputeDistance(cell.position, endpoint1)
@@ -74,39 +45,77 @@ func (fibre *Fibre) FindPivot(cell *Cell) {
 	}
 }
 
-// ComputePhi: Calculates the new angle between the fibre pivot and the center of the cell
+// ComputePhi: Calculates the angle of rotation between the fibre pivot and the center of the cell
 // Input:
-// theta (float64): The initial angle between the fibre pivot and the center of the cell
-func ComputePhi(fibre *Fibre, cell *Cell, theta, d, D, S float64) float64 {
-	alignFactor := (1 - 0.1*cell.integrin*(1-S))
+// cell (*Cell): Pointer to the cell object that is acting on the fibre.
+// S (float64): Matrix stiffness.
+// Output:
+// (float64) The angle that the fibre needs to be rotated about its pivot.
+func (f *Fibre) ComputePhi(cell *Cell, S float64) float64 {
+	// phi (angle of rotation) = theta (angle of cell from pivot) - arcsin[(1 - 0.1*integrins*(1-stiffness)*perpendicular distance D) / hypotenuse]
+
+	D := f.FindPerpendicularDistance(cell)       // perpendicular distance from cell to fibre
+	d := ComputeDistance(f.pivot, cell.position) // distance from pivot point to cell
+	I := cell.integrin                           // Percentage of integrins expressed by the cell
+	theta := f.FindTheta(D, d)                   // Angle between fibre and line from pivot to cell.
+
+	alignFactor := (1 - 0.1*I*(1-S))
 	phi := (theta - math.Asin(alignFactor*D/d))
-	return phi
+	rotationSign := f.DetermineRotationDirection(cell)
+	return float64((rotationSign)) * phi
+}
+
+// Determines whether the fibre needs to be rotated in the clockwise (negative) or counter-clockwise (positive) direction
+// to align the fibre closer to the center of the cell acting on the fibre. Returns 1 if rotation should be counter-clockwise,
+// Otherwise returns 0.
+// Input:
+// f (*Fibre) Pointer to the fibre being acted on
+// cell (*Cell) Pointer to the cell acting on the fibre
+// Output:
+// (int) -1 if rotation should be clockwise, 1 if rotation should be clockwise.
+func (f *Fibre) DetermineRotationDirection(cell *Cell) int {
+	nonPivot := f.GetNonPivot() // non-pivot end of the fibre
+
+	m, b := FindLine(f.pivot, cell.position) // y = mx + b of line between pivot and cell center
+	y := EvaluateLineAtX(nonPivot.x, m, b)
+
+	rotation := 1
+	if f.pivot.x < cell.position.x { // If pivot is to the left of cell.
+		if nonPivot.y > y { // Non-Pivot end is above the line from pivot to cell
+			rotation *= -1
+		}
+	} else { // If Pivot is to the right of the cell.
+		if nonPivot.y < y { // Non-Pivot end is below the line from pivot to cell
+			rotation *= -1
+		}
+	}
+	return rotation
 }
 
 // UpdateDirection: Updates the direction of a fibre
-// Input: fibre (*Fibre) pointer to a Fibre object.
-func (fibre *Fibre) UpdateDirection(phi float64) {
-	x := fibre.direction.x
-	y := fibre.direction.y
-	fibre.direction.x = x*math.Cos(phi) - y*math.Sin(phi)
-	fibre.direction.y = x*math.Sin(phi) + y*math.Cos(phi)
+// Input: f (*Fibre) pointer to a Fibre object.
+func (f *Fibre) UpdateDirection(phi float64) {
+	x := f.direction.x
+	y := f.direction.y
+	f.direction.x = x*math.Cos(phi) - y*math.Sin(phi)
+	f.direction.y = x*math.Sin(phi) + y*math.Cos(phi)
 }
 
-//	UpdatePosition: Updates the position of a fibre.
-//
+// UpdatePosition: Updates the position of a fibre.
 // Input: fibre (*Fibre) location of the center of the fibre.
 // Output: None.
-func (fibre *Fibre) UpdatePosition() {
-	magnitude := math.Sqrt(fibre.direction.x*fibre.direction.x + fibre.direction.y*fibre.direction.y)
-	fibre.position.x = fibre.pivot.x + fibre.direction.x*0.5*fibre.length/magnitude
-	fibre.position.y = fibre.pivot.y + fibre.direction.y*0.5*fibre.length/magnitude
+func (f *Fibre) UpdatePosition() {
+	magnitude := math.Sqrt(f.direction.x*f.direction.x + f.direction.y*f.direction.y)
+	f.position.x = f.pivot.x + f.direction.x*0.5*f.length/magnitude
+	f.position.y = f.pivot.y + f.direction.y*0.5*f.length/magnitude
 }
 
 // FindPerpendicularDistance: Finds shortest distance from the center of a cell to a fibre.
 // Input:
 // fibre (*Fibre) Pointer to a fibre object.
 // cell (*Cell) Pointer to a cell object.
-// Output: (float64) The distance from the center of the cell to the fibre.
+// Output:
+// (float64) The distance from the center of the cell to the fibre.
 func (fibre *Fibre) FindPerpendicularDistance(cell *Cell) float64 {
 	// need to find coordinates of pivot point
 	A, B, C := FindHomogenousLine(fibre.position, fibre.pivot)
@@ -159,10 +168,30 @@ func (fibre *Fibre) GetLine() (float64, float64) {
 // Output: (OrderedPair) The non-pivot end of the fibre.
 func (fibre *Fibre) GetNonPivot() OrderedPair {
 	endpoint1, endpoint2 := fibre.GetEndpoints()
-	if fibre.pivot == endpoint1 {
+	if fibre.pivot != endpoint1 {
 		return endpoint1
 	}
 	return endpoint2
+}
+
+// FindTheta: Find the angle between the line describing the fibre and the line between the pivot and the cell
+func (f *Fibre) FindTheta(opposite, hypotenuse float64) float64 {
+	theta := math.Asin(opposite / hypotenuse) // theta = arcsin(opposite / hypotenuse)
+	if theta != theta {
+		panic("Error in FindTheta: arcsin returned NaN.")
+	}
+	return theta
+}
+
+// CopyFibre: Returns a pointer to a copy of a Fibre object.
+func (f *Fibre) CopyFibre() *Fibre {
+	var newFibre Fibre
+	newFibre.direction = f.direction
+	newFibre.length = f.length
+	newFibre.position = f.position
+	newFibre.width = f.width
+	newFibre.pivot = f.pivot
+	return &newFibre
 }
 
 // This is just a dead function... Wth is it even doing?
